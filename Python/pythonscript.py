12de41e67bcae3bd96c8e6bd7353564de19cc801
AbhishekPattnaik04 <58695787+AbhishekPattnaik04@users.noreply.github.com>
2020-05-22 18:22:20 +0530

    Update pythonscript.py

--- Python/pythonscript.py
+++ Python/pythonscript.py
@@ -2,7 +2,7 @@
 
 from python_terraform import *
 
-tf = Terraform(working_dir='/var/lib/jenkins/workspace/Terraform-Python/Python')
+tf = Terraform(working_dir='/var/lib/jenkins/workspace/Terraform-Python/Python', terraform_bin_path='/root/terraform-bin')
 tf.plan(no_color=IsFlagged, refresh=False, capture_output=True)
 approve = {"auto-approve": True}
 print(tf.init(reconfigure=True))
