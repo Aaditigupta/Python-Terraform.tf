87c0d13fccabcf09c90542cc9678f241985ee79c
AbhishekPattnaik04 <58695787+AbhishekPattnaik04@users.noreply.github.com>
2020-05-28 16:22:08 +0530

    Update pythonscript.py

--- Python/pythonscript.py
+++ Python/pythonscript.py
@@ -14,7 +14,7 @@ args = vars(ap.parse_args())
 enter = int(args['arg1'])
 instance_id = args['arg2']
 region = args['arg3']
-tf = Terraform(working_dir='/home/test', variables={'count':enter, 'aws-region':region , 'AMIs': instance_id })
+tf = Terraform(working_dir='/var/lib/jenkins/workspace/Terraform-Python/Python', variables={'count':enter, 'aws-region':region , 'AMIs': instance_id })
 tf.plan(no_color=IsFlagged, refresh=False, capture_output=True, out=plan.out)
 #approve = {"auto-approve": True}
 print(tf.init(reconfigure=True))
