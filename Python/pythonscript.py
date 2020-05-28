3f224af43e29d635a5eac60b4f827658acbf6b6f
AbhishekPattnaik04 <58695787+AbhishekPattnaik04@users.noreply.github.com>
2020-05-28 11:00:05 +0530

    Update pythonscript.py

--- Python/pythonscript.py
+++ Python/pythonscript.py
@@ -1,7 +1,7 @@
 
 from python_terraform import *
 
-tf = Terraform(working_dir='/var/lib/jenkins/workspace/Terraform-Python/infra')
+tf = Terraform(working_dir='/var/lib/jenkins/workspace/Terraform-Python/Python')
 tf.plan()
 print(tf.init(reconfigure=True))
 print(tf.apply(skip_plan = True))
