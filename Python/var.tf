1715df8db7da8927ae3dc8f50789a362a01fee3e
AbhishekPattnaik04 <58695787+AbhishekPattnaik04@users.noreply.github.com>
2020-05-28 16:08:49 +0530

    Update var.tf

--- Python/var.tf
+++ Python/var.tf
@@ -51,10 +51,7 @@ variable "private_subnet-d-cidr" {
 }
 
 variable "AMIS" {
-  type = "map"
-  default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-844e0bf7"
-  }
+  type = "string"
+  default = "ami-085925f297f89fce1"
+   
 }
