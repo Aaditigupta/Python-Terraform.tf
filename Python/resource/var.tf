d3b966bc6ff19dc73775aa29cdbfd730d582bd21
AbhishekPattnaik04 <58695787+AbhishekPattnaik04@users.noreply.github.com>
2020-06-30 19:19:37 +0530

    Update var.tf

--- Python/resource/var.tf
+++ Python/resource/var.tf
@@ -1,1 +1,62 @@
+variable "num" {
+default=2
+}
+variable "vpc_id" {
+  type="string"
+  default = "vpc-0cebde6cf3db0f007"
+
+}
+
+variable "aws-region" {
+    type = "string"
+    default = "us-east-1"
+}
+
+variable "myVPC-cidr" {
+    type = "string"
+    default = "192.168.0.0/16"
+}
+
+variable "public_subnet-a-cidr" {
+    type = "string"
+    default = "192.168.0.0/28"
+}
+
+
+variable "public_subnet-b-cidr" {
+    type = "string"
+    default = "192.168.0.16/28"
+}
+
+
+variable "private_subnet-a-cidr" {
+    type = "string"
+    default = "192.168.0.32/28"
+}
+
+variable "private_subnet-b-cidr" {
+    type = "string"
+    default = "192.168.0.48/28"
+}
+variable "private_subnet-c-cidr" {
+    type = "string"
+    default = "192.168.0.64/28"
+}
+variable "private_subnet-d-cidr" {
+    type = "string"
+    default = "192.168.0.80/28"
+}
+variable "demo"{
+    type = "string"
+    default = "vpc-0cebde6cf3db0f007"
+}
+
+
+variable "AMIS" {
+type = "map"
+  default =  {
+    us-east-1 = "ami-085925f297f89fce1"
+    us-east-2 = "ami-0a54aef4ef3b5f881"
+    }
+}
 
