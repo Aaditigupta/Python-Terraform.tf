810c7d0bddc301501a18435d3fabd9843e587e4c
AbhishekPattnaik04 <58695787+AbhishekPattnaik04@users.noreply.github.com>
2020-07-01 11:12:09 +0530

    Update var.tf

--- Python/var.tf
+++ Python/var.tf
@@ -50,10 +50,6 @@ variable "private_subnet-d-cidr" {
     default = "192.168.0.80/28"
 }
 
-variable "enable_vpc" {
-  description = "if set to true,create new vpc"
-  type = bool
- }
 variable "AMIS" {
   type = "map"
   default =  {
