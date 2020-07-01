08440e5f7534d138e91fa79accc8ca3247ac4118
AbhishekPattnaik04 <58695787+AbhishekPattnaik04@users.noreply.github.com>
2020-07-01 11:11:14 +0530

    Update pythonscript.py

--- Python/pythonscript.py
+++ Python/pythonscript.py
@@ -1,7 +1,6 @@
 
 from python_terraform import *
 import argparse
-import time
 ap = argparse.ArgumentParser()
 ap.add_argument("-a", "--arg1", required=True,
     help="path to argument1")
@@ -9,17 +8,31 @@ ap.add_argument("-b", "--arg2", required=True,
     help="path to argument2")
 ap.add_argument("-c", "--arg3", required=True,
     help="path to argument3")
-args = vars(ap.parse_args())
+ap.add_argument("-d", "--arg4", required=True,
+    help="path to argument4")
+ap.add_argument("-e", "--arg5", required=True,
+    help="path to argument5")
 
+    
+args = vars(ap.parse_args())
 enter = int(args['arg1'])
 instance_id = args['arg2']
 region = args['arg3']
 d = {}
 d[region] = instance_id
-tf = Terraform(working_dir='/var/lib/jenkins/workspace/Terraform-Python/Python', variables={'count':enter, 'aws-region':region , 'AMIs': d })
-tf.plan(no_color=IsFlagged, refresh=False, capture_output=True)
-#approve = {"auto-approve": True}
-print(tf.init(reconfigure=True))
-print(tf.apply(skip_plan = True))
-#time.sleep(300)
-#tf.destroy()
+state = int(args['arg4'])
+
+if bool(state) == False:
+    vpc_id = args['arg5']
+if bool(state) == True:
+    tf = Terraform(working_dir='/var/lib/jenkins/workspace/infra Provisioning/Python', variables={'count':enter, 'aws-region':region , 'AMIs': d})
+    tf.plan(no_color=IsFlagged, refresh=False, capture_output=True)
+    #approve = {"auto-approve": True}
+    print(tf.init(reconfigure=True))
+    print(tf.apply(skip_plan = True))
+else:
+    tf = Terraform(working_dir='/var/lib/jenkins/workspace/infra Provisioning/Python/resource', variables={'count':enter, 'aws-region':region , 'AMIs': d, 'vpc_id': vpc_id})
+    tf.plan(no_color=IsFlagged, refresh=False, capture_output=True)
+    #approve = {"auto-approve": True}
+    print(tf.init(reconfigure=True))
+    print(tf.apply(skip_plan = True))
